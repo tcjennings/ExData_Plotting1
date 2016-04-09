e08b0cf21c3baf81e2663fc73667442fcd38b692
Toby Jennings <toby@tobyjennings.com>
2016-04-09 13:43:51 -0500

    Script and plot for plot3

--- plot3.R
+++ plot3.R
@@ -1,5 +1,34 @@
 #This script creates plot3.png
 
 #First, use the 00_data_fetcher.R script to download and unpack the source data if necessary. This script also sets the input_file variable.
-source("./00_data_fetcher.R")
+if (! exists("dt") ) {
+  source("./00_data_fetcher.R")
+}
 
+png(filename="plot3.png",height=480,width=480)
+
+plot( x=dt$DateTime, y=dt$Sub_metering_1,
+      type="l", #line
+      main="",xlab="",ylab="",
+      col="black",
+      cex.axis=0.75
+      )
+
+lines( x=dt$DateTime, y=dt$Sub_metering_2,
+      col="red"
+)
+
+lines( x=dt$DateTime, y=dt$Sub_metering_3,
+      col="blue"
+)
+
+title(ylab="Energy sub metering",
+      cex.lab=0.80)
+
+legend("topright",
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=1,
+       cex=0.75)
+
+dev.off()
