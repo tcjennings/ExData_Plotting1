5676d1950bd78064d198d49d2e199898ef3f6411
Toby Jennings <toby@tobyjennings.com>
2016-04-09 12:20:56 -0500

    Plot1 script and image complete.

--- plot1.R
+++ plot1.R
@@ -3,3 +3,20 @@
 #First, use the 00_data_fetcher.R script to download and unpack the source data if necessary. This script also sets the input_file variable.
 source("./00_data_fetcher.R")
 
+#Save the file as Plot1.png
+png(filename="plot1.png",width=480,height=480)
+
+#Plot1 is a histogram of global active power in kilowatts
+hist(dt$Global_active_power,
+     col="red",
+     main=NULL,xlab=NULL,ylab=NULL,
+     cex.axis=0.75)
+
+#Label the title and axes
+title(main="Global Active Power",
+      xlab="Global Active Power (kilowatts)",
+      ylab="Frequency",
+      cex.lab=0.80,
+      cex.main=0.90)
+
+dev.off()
