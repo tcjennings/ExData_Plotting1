2dece1ea8387ca9650b3c208bae537c743c9de47
Toby Jennings <toby@tobyjennings.com>
2016-04-09 17:28:17 -0500

    Add comments.

--- plot3.R
+++ plot3.R
@@ -6,8 +6,13 @@ if (! exists("dt") ) {
   source("./00_data_fetcher.R")
 }
 
+#Create a PNG for plot3
 png(filename="plot3.png",height=480,width=480)
 
+#Plot3 is a line plot with three data series for Sub_Metering_1/2/3
+# It includes a legend in the upper right corner
+
+#Start plot with first line in black
 plot( x=dt$DateTime, y=dt$Sub_metering_1,
       type="l", #line
       main="",xlab="",ylab="",
@@ -15,21 +20,25 @@ plot( x=dt$DateTime, y=dt$Sub_metering_1,
       cex.axis=0.75
       )
 
+#Add second line in red
 lines( x=dt$DateTime, y=dt$Sub_metering_2,
       col="red"
 )
 
+#Add third line in blue
 lines( x=dt$DateTime, y=dt$Sub_metering_3,
       col="blue"
 )
 
+#Add plot title
 title(ylab="Energy sub metering",
       cex.lab=0.80)
 
+#Add plot legend
 legend("topright",
        c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        col=c("black","red","blue"),
-       lty=1,
+       lty=1, #Solid line style
        cex=0.75)
 
 dev.off()
